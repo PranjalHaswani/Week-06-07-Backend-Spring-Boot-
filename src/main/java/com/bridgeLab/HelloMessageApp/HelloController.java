3ae401e6906fffb1812b52205ed4bc9862ced0f5
Pranjal Haswani <pranjalhaswani00@gmail.com>
2025-02-21 15:18:16 +0530

    [Pranjal_Haswani]add. Message from Parameter Application.

--- src/main/java/com/bridgeLab/HelloMessageApp/HelloController.java
+++ src/main/java/com/bridgeLab/HelloMessageApp/HelloController.java
@@ -1,6 +1,8 @@
 package com.bridgeLab.HelloMessageApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,13 @@ public class HelloController {
     @GetMapping("/helloPrint")
     public String UC1(){
         return "Hello BridgeLabz";
+    }
+    @GetMapping("/hello/query")
+    public String UC2(@RequestParam String name) {
+        return "Hello" + name + " from BridgeLabz";
+    }
+    @GetMapping("/hello/param/{name}")
+    public String UC3(@PathVariable("name") String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
