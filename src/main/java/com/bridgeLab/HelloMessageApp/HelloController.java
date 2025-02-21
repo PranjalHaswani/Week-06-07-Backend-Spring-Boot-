3363fa6b8c81387161da3f2ace03c790328b553c
Pranjal Haswani <pranjalhaswani00@gmail.com>
2025-02-21 14:04:40 +0530

    [Pranjal_Haswani]add. Hello Message With Mark Application.

--- src/main/java/com/bridgeLab/HelloMessageApp/HelloController.java
+++ src/main/java/com/bridgeLab/HelloMessageApp/HelloController.java
@@ -1,6 +1,7 @@
 package com.bridgeLab.HelloMessageApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/helloPrint")
     public String UC1(){
         return "Hello BridgeLabz";
+
+    }
+    @GetMapping("/hello/query")
+    public String UC2(@RequestParam String name){
+        return "Hello" + name +" from BridgeLabz";
     }
 }
